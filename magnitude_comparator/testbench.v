923f862e60e5bf3ab3a6c42eb63bc3fba49f9aa0
RiyaRkk <riya.rkk2@gmail.com>
2025-07-26 23:59:35 +0530

    Create testbench.v

---
+++ magnitude_comparator/testbench.v
@@ -0,0 +1,19 @@
+module magnitude_comparator_tb;
+  reg [3:0]a,b;
+  wire out;
+  comparator_gatelevel uut(.a(a), .b(b), .out(out));
+  initial begin 
+    $monitor("a=%b, b=%b, out=%b",a,b,out);
+     a=4'b0000; b=4'b0000; #10;
+  a=4'b0001; b=4'b0000; #10;
+  a=4'b0010; b=4'b0001; #10;
+  a=4'b0011; b=4'b0100; #10;
+  a=4'b1111; b=4'b1110; #10;
+  a=4'b1111; b=4'b1111; #10;
+  a=4'b1000; b=4'b1000; #10;
+  a=4'b0101; b=4'b0110; #10;
+  a=4'b0110; b=4'b0101; #10;
+  a=4'b0000; b=4'b1111; #10;
+    $finish;
+  end
+endmodule
